921b90572ef32f805f02c38ba27d86c7ae0d2358
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-17 20:46:43 +0700

    fungsi untuk sebagai entry point navigasi, menggunakan NavHost untuk mendefinisikan tujuan navigasi HomeMhsView dan InsertMhsView

--- app/src/main/java/com/dev/roomlocaldb/ui/navigation/PenggelolaHalaman.kt
+++ app/src/main/java/com/dev/roomlocaldb/ui/navigation/PenggelolaHalaman.kt
@@ -5,26 +5,56 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
 import androidx.navigation.compose.rememberNavController
 import com.dev.roomlocaldb.ui.view.mahasiswa.DestinasiInsert
 import com.dev.roomlocaldb.ui.view.mahasiswa.InsertMhsView
+import com.dev.roomlocaldb.ui.view.mahasiswa.DetailMhsView
+import com.dev.roomlocaldb.ui.view.mahasiswa.HomeMhsView
+import com.dev.roomlocaldb.ui.view.mahasiswa.UpdateMhsView
+
 
 @Composable
 fun PenggelolaHalaman(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier
-){
+) {
     NavHost(
         navController = navController,
-        startDestination = DestinasiInsert.route){
+        startDestination = DestinasiHome.route) {
+
         composable(
-            route = DestinasiInsert.route
-        ){
-            InsertMhsView(
-                onBack = {}, onNavigate = {}
+            route = DestinasiHome.route
+        ) {
+            HomeMhsView(
+                onDetailClick = {nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+                    println(
+                        "PengelolaHalaman: nim = $nim"
+                    )
+                },
+                onAddMhs = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+                modifier = modifier
             )
         }
+
+        composable(
+            route = DestinasiInsert.route
+        ) {
+            InsertMhsView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier,
+            )
+        }
+
+
     }
-
-
 }
