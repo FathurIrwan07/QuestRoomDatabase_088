bc1ae41997cdd03975e82fd81a5fba27343f816f
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-17 09:17:40 +0700

    mengimplementasikan fungsi-fungsi RepositoryMhs dengan menggunakan DAO

--- app/src/main/java/com/dev/roomlocaldb/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/dev/roomlocaldb/repository/LocalRepositoryMhs.kt
@@ -2,11 +2,28 @@ package com.dev.roomlocaldb.repository
 
 import com.dev.roomlocaldb.data.dao.MahasiswaDao
 import com.dev.roomlocaldb.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs(
     private val mahasiswaDao: MahasiswaDao
 ) : RepositoryMhs {
-    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
+    override suspend fun insertMhs(mahasiswa: Mahasiswa){
         mahasiswaDao.insertMahasiswa(mahasiswa)
+    }
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa> {
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.updateMahasisa(mahasiswa)
     }
 }
