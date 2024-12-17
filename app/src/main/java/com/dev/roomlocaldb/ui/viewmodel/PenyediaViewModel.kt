d18f720efc2c63326f427acccfa3a037af190523
FathurIrwan07 <muhamadfathurirwan@gmail.com>
2024-12-17 14:58:12 +0700

    menambahkan inisialisasi ViewModel ke dalam konfigurasi aplikasi pada object PenyediaViewModel

--- app/src/main/java/com/dev/roomlocaldb/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/dev/roomlocaldb/ui/viewmodel/PenyediaViewModel.kt
@@ -1,20 +1,40 @@
 package com.dev.roomlocaldb.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.dev.roomlocaldb.KrsApp
 
 object PenyediaViewModel {
+
     val Factory = viewModelFactory {
         initializer {
             MahasiswaViewModel(
-                KrsApp().containerApp.repositoryMhs
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            HomeMhsViewModel(
+                krsApp().containerApp.repositoryMhs
+            )
+        }
+        initializer {
+            DetailMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs,
+            )
+        }
+        initializer {
+            UpdateMhsViewModel(
+                createSavedStateHandle(),
+                krsApp().containerApp.repositoryMhs
             )
         }
     }
 }
 
-fun CreationExtras.KrsApp(): KrsApp =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as KrsApp)
+
+fun CreationExtras.krsApp(): KrsApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KrsApp)
